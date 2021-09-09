802c15e5a892643dfac772c760311c106a4b9da9
Laishuxin <15622971239@163.com>
2021-09-09 16:43:47 +0800

    chore: 取消路由前缀

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,14 @@ import { useAuth } from 'context/auth-context'
 import { UnAuthenticatedApp } from 'unauthenticated-app'
 import { FullPageErrorFeedback } from './components/common/lib'
 import { ErrorBoundary } from './components/common/error-boundary'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { ROUTER_PREFIX } from 'config'
-import { NotFound } from 'components/common/404'
 import 'App.css'
 
 function App() {
   const { user } = useAuth()
-  const Main = user ? <AuthenticatedApp /> : <UnAuthenticatedApp />
   return (
     <div className='App'>
       <ErrorBoundary fallbackRender={FullPageErrorFeedback}>
-        <Routes>
-          <Route path={ROUTER_PREFIX + '/*'} element={Main} />
-          <Navigate to={ROUTER_PREFIX} />
-          <Route path={'/*'} element={<NotFound />} />
-        </Routes>
+        {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
       </ErrorBoundary>
     </div>
   )
